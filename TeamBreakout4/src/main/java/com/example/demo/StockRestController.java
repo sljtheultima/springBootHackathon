31b292290695b548bfa903ff43b610eed2db1fe4
gohc0079 <raygohcr@gmail.com>
2020-09-04 18:03:21 +0000

    Code refactor

--- TeamBreakout4/src/main/java/com/example/demo/StockRestController.java
+++ TeamBreakout4/src/main/java/com/example/demo/StockRestController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
-import org.apache.coyote.Response;
-import org.json.JSONObject;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.core.ParameterizedTypeReference;
@@ -23,44 +25,33 @@ public class StockRestController {
     ApiProperties apiProperties;
 
     @GetMapping(value = "/getEarnings")
-    public ResponseEntity<Earnings> getEarningsData(@RequestParam("username") String username
+    public ResponseEntity<JsonNode> getEarningsData(@RequestParam("username") String username
             ,@RequestParam( value = "size",defaultValue = "10") int size
-            ,@RequestParam(value = "region",defaultValue = "US") String region){
+            ,@RequestParam(value = "region",defaultValue = "US") String region) throws JsonProcessingException {
 
-        HttpHeaders headers = new HttpHeaders();
+
         String url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-earnings?startDate=1585155600000&endDate=1589475600000";
-        headers.setContentType(MediaType.APPLICATION_JSON);
+
 
         UriComponentsBuilder builder  = UriComponentsBuilder.fromHttpUrl(url)
                 .queryParam("size",size)
                 .queryParam("region",region);
 
 
-        headers.add(apiProperties.getKey_name(), apiProperties.getKey_value());
-        headers.add(apiProperties.getHost_name(),apiProperties.getHost_value());
-
-
-
-        HttpEntity entity = new HttpEntity(headers);
-        ResponseEntity<Earnings> responseEntity = template.exchange(builder.toUriString(), HttpMethod.GET,entity,Earnings.class);
-
-
-        return responseEntity;
+        return ResponseEntity.ok(getJsonObject(builder.toUriString()));
 
     }
 
     @GetMapping("/historicalData")
-   public ResponseEntity<HistoricalData> getHistoricalData(@RequestParam("username") String username ,
+   public ResponseEntity<JsonNode> getHistoricalData(@RequestParam("username") String username ,
                                                                  @RequestParam(value = "period1", defaultValue ="154648400") Long period1,
                                                                  @RequestParam(value = "period2",defaultValue = "1562086800") Long period2,
                                                                  @RequestParam(value = "symbol",defaultValue = "AMRN")String symbol,
                                                                  @RequestParam(value = "frequency",defaultValue = "1d",required = false)String freq,
-                                                                 @RequestParam(value = "filter",defaultValue = "history",required = false) String filter){
+                                                                 @RequestParam(value = "filter",defaultValue = "history",required = false) String filter) throws JsonProcessingException {
 
-        HttpHeaders headers = new HttpHeaders();
+
         String url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data";
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
         UriComponentsBuilder builder  = UriComponentsBuilder.fromHttpUrl(url)
                 .queryParam("period1",period1)
                 .queryParam("period2",period2)
@@ -69,16 +60,22 @@ public class StockRestController {
                 .queryParam("filter",filter);
 
 
-        headers.add(apiProperties.getKey_name(), apiProperties.getKey_value());
+        return ResponseEntity.ok(getJsonObject(builder.toUriString()));
+
+
+    }
+    
+    public HttpEntity httpHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
         headers.add(apiProperties.getHost_name(),apiProperties.getHost_value());
-        HttpEntity entity = new HttpEntity(headers);
+        headers.add(apiProperties.getKey_name(),apiProperties.getKey_value());
+        return new HttpEntity(headers);
+        
+    }
 
-
-        ResponseEntity<HistoricalData> responseEntity = template.exchange(builder.toUriString(), HttpMethod.GET,entity,HistoricalData.class);
-        System.out.println(responseEntity);
-        return responseEntity;
-
-
+    public JsonNode getJsonObject(String urlStr) throws JsonProcessingException {
+        return new ObjectMapper().readTree(template.exchange(urlStr, HttpMethod.GET,httpHeader(),String.class).getBody().toString());
     }
 
 
