92b829d50384cfdffbeaed5d95178c9b6194394a
wongxinxian <xinxian_wong@hotmail.com>
2020-09-04 09:53:01 +0000

    changes to UserRestController

--- TeamBreakout4/src/main/java/com/example/demo/UserRestController.java
+++ TeamBreakout4/src/main/java/com/example/demo/UserRestController.java
@@ -1,10 +1,10 @@
 package com.example.demo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,18 +14,53 @@ import java.util.stream.Collectors;
 public class UserRestController{
 
     @Autowired
-    private Map<Integer, User> userlist;
+    private UserService service;
 
-    @Autowired
-    private Userservice service;
+    @GetMapping(value="/AllUsers", produces={"application/json","application/xml"})
+    public List<User> getAllUsers() {
+        return service.getAllUsers();
+    }
 
-    @GetMapping(value="/items", produces={"application/json","application/xml"})
-    public List<Item> getAllUsers() {
-        return service.getAllUsers()
-                .keySet()
-                .stream()
-                .map(id -> userlist.get(id))
-                .collect(Collectors.toList());
+    @GetMapping(value="/getUserByUserName/{username}", produces={"application/json","application/xml"})
+    public List<User> getUserByUserName(@PathVariable String username) {
+        return service.getUserByUserName(username);
+    }
+
+    @GetMapping(value="/getUserByFullName/{fullname}", produces={"application/json","application/xml"})
+    public List<User> getUserByFullName(@PathVariable String fullname) {
+        return service.getUserByUserName(fullname);
+    }
+
+    @PostMapping(value="/insertNewUser",consumes={"application/json","application/xml"},
+            produces={"application/json","application/xml"})
+    public ResponseEntity<User> addUser (@RequestBody User user) {
+        System.out.println("Adding " + user);
+        service.insertNewUser(user);
+        URI uri = URI.create("/user/" + user.getUsername());
+        return ResponseEntity.created(uri).body(user);
+    }
+
+    @PutMapping(value="/updateUserByUsername/{username}", consumes={"application/json","application/xml"})
+    public ResponseEntity modifyUser(@PathVariable String username, @RequestBody User user) {
+        if (service.getUserByUserName(username) == null)
+            return ResponseEntity.notFound().build();
+        else {
+            System.out.println("Updating details of " + username);
+            service.updateUserByUserName(user);
+            return ResponseEntity.ok().build();
+        }
+    }
+
+    @DeleteMapping("/deleteUserByUserName/{username}")
+    public ResponseEntity deleteUser(@PathVariable String username) {
+        if (service.getUserByUserName(username) == null)
+            return ResponseEntity.notFound().build();
+        else {
+            System.out.println("Deleting details of " + username);
+            service.deleteUserByUserName(username);
+            return ResponseEntity.ok().build();
+        }
     }
 
 }
+
