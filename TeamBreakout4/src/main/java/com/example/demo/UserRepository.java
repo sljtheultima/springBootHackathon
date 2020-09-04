9fba56ef1ae3ea775731d94d6de43d1fd12c0142
Li Jin <sljtheultima@hotmail.com>
2020-09-04 06:12:14 +0000

    Added base files for mongodb requirements

---
+++ TeamBreakout4/src/main/java/com/example/demo/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository{
+
+//    List<User> findUserByUsername(String username);
+//
+//    List<User> findUserByUsername(String username);
+
+
+}
