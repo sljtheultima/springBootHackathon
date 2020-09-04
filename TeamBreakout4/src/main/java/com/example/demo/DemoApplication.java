ddc5aa22531cd418a594dfe9e6f22e09b7447811
gohc0079 <raygohcr@gmail.com>
2020-09-04 07:21:02 +0000

    updated url with extra headers and params

--- TeamBreakout4/src/main/java/com/example/demo/DemoApplication.java
+++ TeamBreakout4/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class DemoApplication {
--- TeamBreakout4/src/main/java/com/example/demo/StockRestController.java
+++ TeamBreakout4/src/main/java/com/example/demo/StockRestController.java
@@ -6,6 +6,7 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.List;
 
@@ -20,10 +21,16 @@ public class StockRestController {
     ApiProperties apiProperties;
 
     @GetMapping(value = "/getEarnings")
-    public ResponseEntity<Earnings> getEarningsData(@RequestParam("username") String username){
+    public ResponseEntity<Earnings> getEarningsData(@RequestParam("username") String username,@RequestParam( value = "size",defaultValue = "10") int size,@RequestParam(value = "region",defaultValue = "US") String region){
         HttpHeaders headers = new HttpHeaders();
-        String url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-earnings?size=10&region=US&startDate=1585155600000&endDate=1589475600000";
+        String url = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/market/get-earnings?startDate=1585155600000&endDate=1589475600000";
         headers.setContentType(MediaType.APPLICATION_JSON);
+
+        UriComponentsBuilder builder  = UriComponentsBuilder.fromHttpUrl(url)
+                .queryParam("size",size)
+                .queryParam("region",region);
+
+
         headers.add(apiProperties.getKey_name(), apiProperties.getKey_value());
         headers.add(apiProperties.getHost_name(),apiProperties.getHost_value());
         ParameterizedTypeReference<List<Earnings>> responseType =
@@ -31,7 +38,7 @@ public class StockRestController {
 
 
         HttpEntity entity = new HttpEntity(headers);
-        ResponseEntity<Earnings> responseEntity = template.exchange(url, HttpMethod.GET,entity,Earnings.class);
+        ResponseEntity<Earnings> responseEntity = template.exchange(builder.toUriString(), HttpMethod.GET,entity,Earnings.class);
         System.out.println(responseEntity.getBody());
         return responseEntity;
 
